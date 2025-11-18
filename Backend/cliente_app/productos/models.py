9e4cfe950a15397f34f3d3c4aa378f11364e5845
Betuel <betuel.arones@tecsup.edu.pe>
2025-11-18 15:44:56 -0500

    Bakend: Modelos Producto y categoria creada

--- Backend/cliente_app/productos/models.py
+++ Backend/cliente_app/productos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey('categorias.Categoria', on_delete=models.CASCADE, related_name='productos')
+    
+    def __str__(self):
+        return self.nombre
